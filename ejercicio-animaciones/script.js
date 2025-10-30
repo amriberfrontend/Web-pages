54fb3c56bce28b21ea80f1d0c0c08a1d491b8789
Angel <amriberfrontend@gmail.com>
2025-10-30 13:03:43 +0100

    Cleaned up unnecessary code

--- ejercicio-animaciones/script.js
+++ ejercicio-animaciones/script.js
@@ -1,19 +1,7 @@
-const bouncingFox = document.getElementById("bouncing-fox")
+const bouncingFox = document.getElementById("bouncing-fox");
 
-// moveRight()
-function moveRight() {
-    let id = null;
-    let posX = bouncingFox.offsetLeft;
-    clearInterval(id);
-    id = setInterval(frame, 0);
+bouncingFox.addEventListener("animationend", showGallery());
 
-    function frame() {
-        if (posX >= window.innerWidth - 108) {
-            clearInterval(id);
-        } else {
-            posX += 3;
-            bouncingFox.style.left = posX + "px";
-        }
-    }
+function showGallery() {
+    
 }
-
