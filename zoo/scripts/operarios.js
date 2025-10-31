2c13d95d309f74f618d6079d9b025c836ea818e9
Angel <amriberfrontend@gmail.com>
2025-10-31 13:50:35 +0100

    started zoo test

---
+++ zoo/scripts/operarios.js
@@ -0,0 +1,25 @@
+
+const images = [
+    "operario01.jpg",
+    "operario02.jpg",
+    "operario03.jpg",
+    "operario04.jpg",
+    "operario05.jpg",
+    "operario06.jpg",
+    "operario07.jpg",
+    "operario08.jpg",
+    "operario09.jpg",
+    "operario10.jpg"
+];
+
+addOperarios();
+
+async function addOperarios() {
+    const gallery = document.getElementById("image-gallery");
+    for (const image of images) {
+        const img = document.createElement("img");
+        img.setAttribute("src", "../images/" + image);
+        gallery.append(img);
+    }
+
+}
